34eeb40030eea46119a6581b5a2e17e407a7d037
Kumaran Chanthrakumar <Kumaran@K.Chan>
2015-04-28 13:01:17 -0400

    I CAN FIND THE NUMBER OF ROWS WOO

--- Search.java
+++ Search.java
@@ -2,19 +2,31 @@ import java.util.Scanner;
 import java.io.File;
 import java.io.FileNotFoundException;
 
-public class  Search{
+public class Search{
 	public static void main(String[] args) throws FileNotFoundException{
 		int cols = 0;
+		int rows = 0;
 
+//reference word-search.txt file that has the wordsearch board and the words to be found
 		File wordsearch = new File(System.getProperty("user.dir") + "/word-search.txt");
-		Scanner columns = new Scanner(wordsearch);
-		
-		Scanner numLines = new Scanner(columns.nextLine());
-		
+
+//Scanners to find the number of columns/rows in the word-search.txt file
+		Scanner numCols = new Scanner(wordsearch);
+		Scanner numRows = new Scanner(wordsearch);
+		Scanner numLines = new Scanner(numCols.nextLine());
+
+//number of columns in the wordsearch
 		while(numLines.hasNext()){
 			numLines.next();
 			cols++;
 		}
 		System.out.println(cols);
+
+//number of rows in the wordsearch. Check to see if the next line is empty, otherwise add 1 to rows.
+		while(!numRows.nextLine().isEmpty()){
+			rows++;
+		}
+		System.out.println(rows);
+		
 	}
 }
