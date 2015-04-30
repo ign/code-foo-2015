bc1982d2195c9da2a6529304ddfd4afb4abb9db0
Kumaran Chanthrakumar <Kumaran@K.Chan>
2015-04-30 14:12:58 -0400

    How to find the specific parts of data :'(

--- API-Pull/java-json/IGNAPI.java
+++ API-Pull/java-json/IGNAPI.java
@@ -17,18 +17,23 @@ public class IGNAPI{
 		return sb.toString();
 	}	
 
-	public static void parseJSON(String url) throws IOException, JSONException{
+	public static JSONObject parseJSON(String url) throws IOException, JSONException{
 	  InputStream is = new URL(url).openStream();
+	  try {
 	  BufferedReader link = new BufferedReader(new InputStreamReader(is, Charset.forName("UTF-8")));
       String object = getURL(link);
       JSONObject obj = new JSONObject(object);
-      int lengthOfObj = obj.getJSONArray("data").length();
-        System.out.println(lengthOfObj);
-    }
+      return obj;
+  } finally {
+  	is.close();
+  }
+}
 
     public static void main(String[] args) throws IOException, JSONException{
-    	parseJSON("http://ign-apis.herokuapp.com/videos");
+    	//parseJSON("http://ign-apis.herokuapp.com/videos");
     	//System.out.println("------------------------------------------------------------");
-        parseJSON("http://ign-apis.herokuapp.com/articles");
+        JSONObject obj = parseJSON("http://ign-apis.herokuapp.com/articles");
+        //System.out.println(obj.toString());
+        System.out.println(obj.get("data"));
     }
 }
