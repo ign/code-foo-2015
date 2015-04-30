16b05cf03464e7d7fdd44a807082935c4363b4bd
Kumaran Chanthrakumar <Kumaran@K.Chan>
2015-04-30 15:41:41 -0400

    Started from the bottom now I'm done with Question 4

--- API-Pull/java-json/IGNAPI.java
+++ API-Pull/java-json/IGNAPI.java
@@ -17,7 +17,8 @@ public class IGNAPI{
 		return sb.toString();
 	}	
 
-	public static JSONObject parseJSON(String url) throws IOException, JSONException{
+//read through json text and filter out needed information.
+	public static void parseJSON(String url) throws IOException, JSONException{
 	  InputStream is = new URL(url).openStream();
 	  BufferedReader link = new BufferedReader(new InputStreamReader(is, Charset.forName("UTF-8")));
       String object = getURL(link);
@@ -26,6 +27,8 @@ public class IGNAPI{
       //Can now get items within data.
       JSONArray array = obj.getJSONArray("data");
 
+
+	  //loop through data and pick out key components
       for(int i=0; i < array.length(); i++){
       	String articleDescription;
       	String videoDescription;
@@ -33,19 +36,20 @@ public class IGNAPI{
       	JSONObject info = array.getJSONObject(i).getJSONObject("metadata");
 
       	if(info.has("headline")){
-      		articleDescription = info.getString("headline");
+      		articleDescription = ("\nARTICLE: " + info.getString("headline"));
       		System.out.println(articleDescription);
+      	}
+      	if(info.has("name")){
+      		videoDescription = ("\nVIDEO: " + info.getString("name") + 
+      			"\n" + "link: " + info.getString("url"));
+      		System.out.println(videoDescription);
       	}
       }
   	is.close();
-    return obj;
 }
 
     public static void main(String[] args) throws IOException, JSONException{
-    	//parseJSON("http://ign-apis.herokuapp.com/videos");
-    	//System.out.println("------------------------------------------------------------");
-        JSONObject obj = parseJSON("http://ign-apis.herokuapp.com/articles");
-        //System.out.println(obj.toString());
-        //System.out.println(obj.get("data"));
+    	parseJSON("http://ign-apis.herokuapp.com/videos");
+        parseJSON("http://ign-apis.herokuapp.com/articles");
     }
 }
