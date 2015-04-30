e4db174849a7c423c7fcf76858be229f50ed9e62
Kumaran Chanthrakumar <Kumaran@K.Chan>
2015-04-30 13:38:59 -0400

    read about JSONObject.  Would make things a lot easier

--- API-Pull/java-json/IGNAPI.java
+++ API-Pull/java-json/IGNAPI.java
@@ -5,6 +5,8 @@ import java.net.URL;
 import java.nio.charset.Charset;
 
 public class IGNAPI{
+
+//convert url into parseable json text
 	public static String getURL(Reader r) throws IOException{
 		StringBuilder sb = new StringBuilder();
         int n;
@@ -19,12 +21,13 @@ public class IGNAPI{
 	  InputStream is = new URL(url).openStream();
 	  BufferedReader link = new BufferedReader(new InputStreamReader(is, Charset.forName("UTF-8")));
       String object = getURL(link);
-        System.out.println(object);
+      JSONObject obj = new JSONObject(object);
+        System.out.println(obj);
     }
 
     public static void main(String[] args) throws IOException, JSONException{
-    	parseJSON("http://ign-apis.herokuapp.com/videos");
-    	System.out.println("------------------------------------------------------------");
+    	//parseJSON("http://ign-apis.herokuapp.com/videos");
+    	//System.out.println("------------------------------------------------------------");
         parseJSON("http://ign-apis.herokuapp.com/articles");
     }
 }
