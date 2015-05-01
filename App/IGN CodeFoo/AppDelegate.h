cfa3613467c97a865226a3323d97b330bafafda7
Stavropoulos Lab <stavropouloslab@new-host.home>
2015-04-30 21:29:57 -0400

    Kumaran: OMG THE TRANSITIONS WORK.  But that was the easy part...

---
+++ App/IGN CodeFoo/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  IGN CodeFoo
+//
+//  Created by Stavropoulos Lab on 4/30/15.
+//  Copyright (c) 2015 Kumaran. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
